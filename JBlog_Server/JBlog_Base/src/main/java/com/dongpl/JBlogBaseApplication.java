1b1f2ccb287207efc76f2e41be036aff28249a9e
GershonWang <986432015@qq.com>
2023-08-21 17:29:43 +0800

    新增工具模块和基础模块

---
+++ JBlog_Server/JBlog_Base/src/main/java/com/dongpl/JBlogBaseApplication.java
@@ -0,0 +1,13 @@
+package com.dongpl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JBlogBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JBlogBaseApplication.class, args);
+    }
+
+}
